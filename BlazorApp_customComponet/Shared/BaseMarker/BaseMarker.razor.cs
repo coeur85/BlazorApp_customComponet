005373af5ddbb2c45bbbd30f345e49bd056f9b06
ahmed magdi <ahmed.magdi@bgomla.com>
2023-09-25 17:59:54 +0300

    Add project files.

---
+++ BlazorApp_customComponet/Shared/BaseMarker/BaseMarker.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorApp_customComponet.Shared.BaseMarker
+{
+    public partial class BaseMarker
+    {
+        private bool rdio_officePicDisable;
+        private bool drp_staticImagesDisable;
+        private bool fileuploadDisable;
+
+
+        private void OfficeImageChange(ChangeEventArgs e)
+        {
+            drp_staticImagesDisable = true;
+            fileuploadDisable = true;
+        }
+        private void StaticImageChange(ChangeEventArgs e)
+        {
+            fileuploadDisable = true;
+            drp_staticImagesDisable = false;
+        }
+        private void CustomeImageChange(ChangeEventArgs e)
+        {
+            fileuploadDisable = false;
+            drp_staticImagesDisable = true;
+        }
+
+    }
+}
